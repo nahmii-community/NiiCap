1a9143b83db219a21204da031050d2d71f54ee37
Hein <heindauven@gmail.com>
2021-10-11 20:45:56 +0200

    Add static building configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,15 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+const dev = process.env.NODE_ENV === 'development';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/niicap',
+		}
 	}
 };
 
